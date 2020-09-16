dc84098d1c2896f0b2e77a4c2dcf9e8cb647251d
Jhonattan <millanjhonattan0@gmail.com>
2020-09-16 19:44:00 -0400

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-import unittest
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
@@ -17,20 +18,32 @@ class NewVisitorTest(unittest.TestCase):
 
 		# He notices the title of the page metions to-do
 		self.assertIn('To-do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-do', header_text)
 
 		# He is given the option to enter a new to-do item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEquals(inputbox.get_attribute('placeholder'), 'Enter new to-do item')
 
 		# He types "Buy food for my dog"
+		inputbox.send_keys('Buy food for my dog')
 		# When he hits enter, the page updates, and now it lists
 		# "1: Buy food for my dog" as the first item in the list
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text == 'Buy food for my dog' for row in rows))
 
 		# He still has the option to enter another item.
 		# He enters "Feed my dog"
 
+		self.fail('Finish the test!')
+
 		# The page updates again, and now shows both items on his list
 
-		# Hewonders whether the site will remember his list. Then he sees
+		# He wonders whether the site will remember his list. Then he sees
 		# that the site has generated a unique URL for him -- there is some
 		# explanatory text to that effect.
 
