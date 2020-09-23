12edc27d9a1e4126c0683b89492136601093877a
Jhonattan <millanjhonattan0@gmail.com>
2020-09-23 17:59:03 -0400

    Add a new method to the unittests to check row contents in item table

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# A guy checks out the to do app homepage
 		self.browser.get('http://127.0.0.1:8000/')
@@ -32,10 +37,7 @@ class NewVisitorTest(unittest.TestCase):
 		# "1: Buy food for my dog" as the first item in the list
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(3)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy food for my dog', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy food for my dog')
 
 		# He still has the option to enter another item.
 		# He enters "Feed my dog"
@@ -45,11 +47,9 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(3)
 
 		# The page updates again, and now shows both items on his list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy food for my dog', [row.text for row in rows])
-		self.assertIn('2: Feed my dog', [row.text for row in rows])
-
+		self.check_for_row_in_list_table('1: Buy food for my dog')
+		self.check_for_row_in_list_table('2: Feed my dog')
+		
 		self.fail('Finish the test!')
 
 
