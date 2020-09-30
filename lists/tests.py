484cd791594cda4ca6d04b4c902dc83683a0955d
Jhonattan <millanjhonattan0@gmail.com>
2020-09-30 17:12:54 -0400

    Adjust models so that items are associated with different lists

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 from django.http import HttpRequest
 from lists.views import home
-from lists.models import Item
+from lists.models import List, Item
 
 class HomePageTests(TestCase):
 	def test_uses_home_template(self):
@@ -15,8 +15,9 @@ class ListViewTests(TestCase):
 		self.assertTemplateUsed(response, 'lists/list.html')
 
 	def test_displays_all_items(self):
-		item1 = Item.objects.create(text='Item 1')
-		item2 = Item.objects.create(text='Item 2')
+		list_ = List.objects.create()
+		item1 = Item.objects.create(text='Item 1', list=list_)
+		item2 = Item.objects.create(text='Item 2', list=list_)
 
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		self.assertContains(response, 'Item 1')
@@ -33,10 +34,15 @@ class NewListTests(TestCase):
 		response = self.client.post(reverse('new_list'), data={'item_text': 'My new item'})
 		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
-class ItemModelTests(TestCase):
+class ListAndItemModelTests(TestCase):
 	def test_saving_and_retrieving_items(self):
-		first_item = Item.objects.create(text='Buy food for my dog.')
-		second_item = Item.objects.create(text='Feed my dog.')
+		list_ = List.objects.create()
+
+		first_item = Item.objects.create(text='Buy food for my dog.', list=list_)
+		second_item = Item.objects.create(text='Feed my dog.',list=list_)
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
 
 		saved_items = Item.objects.all()
 		self.assertEquals(saved_items.count(), 2)
@@ -44,4 +50,6 @@ class ItemModelTests(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'Buy food for my dog.')
+		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, 'Feed my dog.')
+		self.assertEqual(second_saved_item.list, list_)
